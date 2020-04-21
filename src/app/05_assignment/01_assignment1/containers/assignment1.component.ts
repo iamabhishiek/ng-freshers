1760b4cec6479695ce738ea12a3b7023774d4b89
Abhishiek Mahajan <abhishiek.mahajan@nagarro.com>
2020-04-21 23:02:14 +0530

    Assignment 1

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-assignment1',
   templateUrl: './assignment1.component.html',
-  styleUrls: ['./assignment1.component.css']
+  styleUrls: ['./assignment1.component.css'],
 })
 export class Assignment1Component {
-
+  output: string;
+  onInput(input: string) {
+    this.output = input;
+  }
 }
